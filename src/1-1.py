dccb0471394213046a014c96985249b0361d67d7
Lilian Burdianov <Lilian.Burdianov@dubai.jlw.local>
2023-08-15 14:01:14 +0400

    1-1

--- src/1-1.py
+++ src/1-1.py
@@ -1,7 +1,36 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
+# Download and prepare the data
 data_path = "../data/lifesat.csv"
 lifesat = pd.read_csv(data_path)
+
+X = lifesat[["GDP per capita (USD)"]].values
+y = lifesat[["Life satisfaction"]].values
+
+# Visualize the data
+lifesat.plot(
+    kind="scatter",
+    grid=True,
+    x="GDP per capita (USD)",
+    y="Life satisfaction",
+)
+plt.axis([23_500, 62_500, 4, 9])
+plt.show()
+
+# Select a linear model
+model_linear = LinearRegression()
+model_kn = KNeighborsRegressor(n_neighbors=3)
+
+# Train the model
+model_linear.fit(X, y)
+model_kn.fit(X, y)
+
+# Make a prediction for Cyprus
+X_new = [[37_655.2]]
+
+print(model_linear.predict(X_new))
+print(model_kn.predict(X_new))
